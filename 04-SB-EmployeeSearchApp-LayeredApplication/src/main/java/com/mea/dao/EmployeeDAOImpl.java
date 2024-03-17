9409f106379fc9f9f51613fdd524d2ac2157f13d
Rahul Kumar Pandey <rp201291@gmail.com>
2024-03-17 19:14:05 +0530

    First Layered App Mini-Project

---
+++ 04-SB-EmployeeSearchApp-LayeredApplication/src/main/java/com/mea/dao/EmployeeDAOImpl.java
@@ -0,0 +1,74 @@
+package com.mea.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.management.loading.PrivateClassLoader;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.objenesis.instantiator.basic.NewInstanceInstantiator;
+import org.springframework.stereotype.Repository;
+
+import com.mea.model.Employee;
+
+@Repository("empDAO")
+public class EmployeeDAOImpl implements IEmployeeDAO {
+
+	private static final String GET_EMPS_BY_DESGS="SELECT EMPNO,EMPNAME,EMPDESIG,EMPSALARY,EMPDEPTNO FROM EMPLOYEE WHERE EMPDESIG IN(?,?,?) ORDER BY EMPDESIG";
+	
+	@Autowired
+	private DataSource ds;
+	
+	@Override
+	public List<Employee> getEmployeesByDesig(String desg1, String desg2, String desg3) throws Exception {
+
+		List<Employee> list=new ArrayList<Employee>();
+		
+		//try with resource
+		try(//get Pooled jdbc con object
+				Connection con=ds.getConnection();
+				//create the prepared stmt object
+				PreparedStatement ps =con.prepareStatement(GET_EMPS_BY_DESGS);
+				){
+			
+			//set the query param value
+			ps.setString(1, desg1);
+			ps.setString(2, desg2);
+			ps.setString(3, desg3);
+			
+			//execute the query
+			try(ResultSet rs=ps.executeQuery()){
+				//copy the records of ResultSet obj to ArrayList Collection
+				while(rs.next())
+				{
+					Employee emp = new Employee();
+					emp.setEmpNo(rs.getInt(1));
+					emp.setEmpName(rs.getString(2));
+					emp.setEmpDesig(rs.getString(3));
+					emp.setEmpSalary(rs.getFloat(4));
+					emp.setEmpDeptNo(rs.getInt(5));
+					
+					list.add(emp);
+				}
+			}
+			
+		}
+		catch (SQLException se) {
+
+			se.printStackTrace();
+			throw se;
+		}
+		catch (Exception e) {
+
+			e.printStackTrace();
+			throw e;
+		}
+		return list;
+	}
+
+}
---
+++ 04-SB-EmployeeSearchApp-LayeredApplication/src/main/java/com/mea/service/EmployeeMgmtServiceImpl.java
@@ -0,0 +1,29 @@
+package com.mea.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mea.dao.IEmployeeDAO;
+import com.mea.model.Employee;
+
+@Service
+public class EmployeeMgmtServiceImpl implements IEmployeeMgmtService {
+
+	@Autowired
+	private IEmployeeDAO empDAO;
+	
+	@Override
+	public List<Employee> fetchEmployeesByDesgs(String desg1, String desg2, String desg3) throws Exception {
+
+		//convert the desg to UPPERCASE letters
+		desg1=desg1.toUpperCase();
+		desg2=desg2.toUpperCase();
+		desg3=desg3.toUpperCase();
+		
+		List<Employee> list=empDAO.getEmployeesByDesig(desg1, desg2, desg3);
+		return list;
+	}
+
+}
