f150e46df6bc5477849f2086fdfafebac76d1cbf
Rahul Kumar Pandey <rp201291@gmail.com>
2024-03-18 22:43:49 +0530

    Changing DataSource through properties file using spring.datasource.type

--- 05-SB-EmployeeSearchApp-DataSourceAutoConfiguration/src/main/java/com/mea/Application.java
+++ 05-SB-EmployeeSearchApp-DataSourceAutoConfiguration/src/main/java/com/mea/Application.java
@@ -2,16 +2,49 @@ package com.mea;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.mea.controller.EmployeeOperationsController;
 import com.mea.model.Employee;
 
 @SpringBootApplication
 public class Application {
-
+	
+	/*
+	@Bean(name = "c3PoDs")
+	public ComboPooledDataSource createDs() throws Exception{
+		ComboPooledDataSource cds=new ComboPooledDataSource();
+		cds.setDriverClass("oracle.jdbc.driver.OracleDriver");
+		cds.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+		cds.setUser("system");
+		cds.setPassword("Manager");
+		return cds;
+	}
+      */
+	
+	/*
+	@Autowired
+	private Environment env;
+	
+	@Bean(name = "c3PoDs")
+	public ComboPooledDataSource createDs() throws Exception{
+		ComboPooledDataSource cds=new ComboPooledDataSource();
+		cds.setDriverClass(env.getProperty("spring.datasource.driver-class-name"));
+		cds.setJdbcUrl(env.getProperty("spring.datasource.url"));
+		cds.setUser(env.getProperty("spring.datasource.username"));
+		cds.setPassword(env.getProperty("spring.datasource.password"));
+		return cds;
+	}
+	
+	*/
+	
+	
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
 		EmployeeOperationsController controller = ctx.getBean("empController",EmployeeOperationsController.class);
@@ -25,5 +58,7 @@ public class Application {
 		}
 		ctx.close();
 	}
+	
+	
 
 }
