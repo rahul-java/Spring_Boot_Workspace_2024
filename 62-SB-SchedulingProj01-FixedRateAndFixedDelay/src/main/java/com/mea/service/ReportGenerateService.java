5bbd0cb43ea8b60d722f5a98a5fb924a9db53697
Rahul Kumar Pandey <rp201291.java@gmail.com>
2024-06-23 14:44:01 +0530

    Cron Scheduling

--- 62-SB-SchedulingProj01-FixedRateAndFixedDelay/src/main/java/com/mea/service/ReportGenerateService.java
+++ 62-SB-SchedulingProj01-FixedRateAndFixedDelay/src/main/java/com/mea/service/ReportGenerateService.java
@@ -77,7 +77,7 @@ public class ReportGenerateService {
 		System.out.println(":::::::::::============END===============::::::::::::"+new Date());
 	}*/
 	
-	@Scheduled(fixedDelay = 3000)
+	/*@Scheduled(fixedDelay = 3000)
 	public void generateReport1() {
 		System.out.println("ReportGenerateService.generateReport1() ::: Sales Report on:"+new Date()+" Started... Thread Name :::"+Thread.currentThread().getName());
 		System.out.println("--------------------------------------------");
@@ -87,5 +87,48 @@ public class ReportGenerateService {
 	public void generateReport2() {
 		System.out.println("ReportGenerateService.generateReport2() ::: Sales Report on:"+new Date()+" Started... Thread Name :::"+Thread.currentThread().getName());
 		System.out.println("=================================================");
+	}*/
+	
+	/*@Scheduled(cron =  "15 * * * * *") // second minute hour date month WeekDay
+	public void generateReport1() {
+		System.out.println("ReportGenerateService.generateReport1() ::: "+new Date());
+		//Sun Jun 23 14:25:15 IST 2024
+	}*/
+	
+	/*@Scheduled(cron =  "30 27 * * * *") // second minute hour date month WeekDay
+	public void generateReport1() {
+		System.out.println("ReportGenerateService.generateReport1() ::: "+new Date());
+		//Sun Jun 23 14:27:30 IST 2024
+	}*/
+	
+	/*//                  s :m :h :date : everyMonth : 
+	@Scheduled(cron =  "40 30 10 1 * *") // second minute hour date month WeekDay
+	public void generateReport1() {
+		System.out.println("ReportGenerateService.generateReport1() ::: "+new Date());
+		//Sat Jun 01 10:30:40 IST 2024
+	}*/
+	
+	/*@Scheduled(cron =  "40 32 14 23 * *") // second minute hour date month WeekDay
+	public void generateReport1() {
+		System.out.println("ReportGenerateService.generateReport1() ::: "+new Date());
+		//Sun Jun 23 14:32:40 IST 2024
+	}*/
+	
+	/*//every day evening 8:30:20pm task should be executed
+	@Scheduled(cron =  "20 30 20 * * *") // second minute hour date month WeekDay
+	public void generateReport1() {
+		System.out.println("ReportGenerateService.generateReport1() ::: "+new Date());
+		//Sun Jun 23 20:30:30 IST 2024
+	}*/
+	
+	//cron="* * 20 * * *" => every day 8pm each min each sec
+	//cron="10 * 20 * * *" => every day 8pm each min 10 sec
+	
+	
+	@Scheduled(cron =  "10 * 14 * * *") // second minute hour date month WeekDay
+	public void generateReport1() {
+		System.out.println("ReportGenerateService.generateReport1() ::: "+new Date());
+		//Sun Jun 23 14:42:10 IST 2024
+		//Sun Jun 23 14:43:10 IST 2024
 	}
 }
