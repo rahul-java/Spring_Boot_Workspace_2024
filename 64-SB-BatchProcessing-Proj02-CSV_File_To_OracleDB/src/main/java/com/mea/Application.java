e24269cbefbd9f5c67619e0a5445787a2e9a9e73
Rahul Kumar Pandey <rp201291.java@gmail.com>
2024-07-04 22:37:19 +0530

    Batch Processing

--- 64-SB-BatchProcessing-Proj02-CSV_File_To_OracleDB/src/main/java/com/mea/Application.java
+++ 64-SB-BatchProcessing-Proj02-CSV_File_To_OracleDB/src/main/java/com/mea/Application.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Application.main() ::: START");
+		
 		SpringApplication.run(Application.class, args);
+		
+		System.out.println("Application.main() ::: END");
 	}
 
 }
--- 64-SB-BatchProcessing-Proj02-CSV_File_To_OracleDB/src/main/java/com/mea/config/BatchConfig.java
+++ 64-SB-BatchProcessing-Proj02-CSV_File_To_OracleDB/src/main/java/com/mea/config/BatchConfig.java
@@ -1,13 +1,21 @@
 package com.mea.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
@@ -28,7 +36,12 @@ public class BatchConfig {
 	@Autowired
 	private EmployeeItemProcessor processor;
 	
+	@Autowired
+	private DataSource ds;
+	
+	
 	//READER
+	@Bean(name = "reader")
 	public FlatFileItemReader<Employee> createReader(){
 		//create obj of FlatFileItemReader
 		FlatFileItemReader<Employee> reader=new FlatFileItemReader<Employee>();
@@ -42,7 +55,7 @@ public class BatchConfig {
 		tokenizer.setNames("empno","empname","address","salary");
 		//set line tokens to model class object
 		BeanWrapperFieldSetMapper<Employee> fieldSetMapper=new BeanWrapperFieldSetMapper<Employee>();
-		
+		fieldSetMapper.setTargetType(Employee.class);
 		//Add LineTokenizer and FieldSetMapper object to LineMapper
 		lineMapper.setLineTokenizer(tokenizer);
 		lineMapper.setFieldSetMapper(fieldSetMapper);
@@ -53,6 +66,42 @@ public class BatchConfig {
 		//return the Reader object
 		
 		
-		return null;
+		return reader;
+	}
+	
+	@Bean(name = "writer")
+	public JdbcBatchItemWriter<Employee> createWriter(){
+		//create object for JdbcBatchItemWriter
+		JdbcBatchItemWriter<Employee> writer=new JdbcBatchItemWriter<Employee>();
+		//set dataSource to writer object
+		writer.setDataSource(ds);
+		//set the sql query
+		writer.setSql("INSERT INTO BATCH_EMPLOYEE VALUES(:empno,:empname,:address,:salary,:grossSalary,:netSalary)");
+		//set model class obj to insert query param values
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Employee>());
+		
+		
+		return writer;
+	}
+	
+	@Bean(name = "step1")
+	public Step createStep1() {
+		
+		return stepFactory.get("step1")
+				.<Employee,Employee>chunk(3)
+				.reader(createReader())
+				.writer(createWriter())
+				.processor(processor)
+				.build();
+	}
+	
+	@Bean(name = "job1")
+	public Job createJob() {
+		
+		return jobFactory.get("job1")
+				.incrementer(new RunIdIncrementer())
+				.listener(listener)
+				.start(createStep1())
+				.build();
 	}
 }
