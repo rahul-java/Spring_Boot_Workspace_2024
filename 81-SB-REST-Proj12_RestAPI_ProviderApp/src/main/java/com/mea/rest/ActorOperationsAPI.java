ce3ef56730b3e1c0a048fb4a51708e495b0a2ec7
Rahul Kumar Pandey <rp201291.java@gmail.com>
2024-07-21 00:04:49 +0530

    Consumer API

--- 81-SB-REST-Proj12_RestAPI_ProviderApp/src/main/java/com/mea/rest/ActorOperationsAPI.java
+++ 81-SB-REST-Proj12_RestAPI_ProviderApp/src/main/java/com/mea/rest/ActorOperationsAPI.java
@@ -3,6 +3,7 @@ package com.mea.rest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,10 @@ public class ActorOperationsAPI {
 	@GetMapping("/wish")
 	public ResponseEntity<String> showResultMsg(){
 		return new ResponseEntity<String>("Good Morning",HttpStatus.OK);
+	}
+	
+	@GetMapping("/wish/{id}/{name}")
+	public ResponseEntity<String> showResultMsg1(@PathVariable Integer id,@PathVariable String name){
+		return new ResponseEntity<String>("Good Morning : "+id+" ::: "+name,HttpStatus.OK);
 	}
 }
--- 82-SB-REST-Proj12_RestAPI_ConsumerApp/src/main/java/com/mea/runner/ActorApiConsumerTestRunner2.java
+++ 82-SB-REST-Proj12_RestAPI_ConsumerApp/src/main/java/com/mea/runner/ActorApiConsumerTestRunner2.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-public class ActorApiConsumerTestRunner implements CommandLineRunner {
+public class ActorApiConsumerTestRunner2 implements CommandLineRunner {
 
 	@Autowired
 	private RestTemplate restTemplate;
+	
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -18,7 +19,7 @@ public class ActorApiConsumerTestRunner implements CommandLineRunner {
 		//RestTemplate restTemplate = new RestTemplate();
 		
 		//prepare base url
-		String baseUrl="http://localhost:4041/providerApp/actor-api/wish";
+		//String baseUrl="http://localhost:4041/providerApp/actor-api/wish";
 		//String baseUrl="http://localhost:8080/81-SB-REST-Proj12_RestAPI_ProviderApp/actor-api/wish";
 		
 		//send get mode req to consumer
@@ -28,9 +29,13 @@ public class ActorApiConsumerTestRunner implements CommandLineRunner {
 		System.out.println("Response Status Code ::: "+responseEntity.getStatusCode());
 		*/
 
-		String response = restTemplate.getForObject(baseUrl, String.class);
-		System.out.println("Response Content ::: "+response);		
+		/*String response = restTemplate.getForObject(baseUrl, String.class);
+		System.out.println("Response Content ::: "+response);
+		*/
 		
+		String baseUrl2="http://localhost:4041/providerApp/actor-api/wish/{id}/{name}";
+		String response = restTemplate.getForObject(baseUrl2, String.class,1001,"Raja");
+		System.out.println("Response Content ::: "+response);		
 		
 	}
 
