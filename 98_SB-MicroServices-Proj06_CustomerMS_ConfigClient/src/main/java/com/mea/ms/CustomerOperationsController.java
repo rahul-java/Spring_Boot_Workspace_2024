0f5aba8fa4a80e3fac07b91e220fe3550df5c3a1
Rahul Kumar Pandey <rp201291.java@gmail.com>
2024-08-31 21:50:26 +0530

    MicroServices : Config Server External Git File

---
+++ 98_SB-MicroServices-Proj06_CustomerMS_ConfigClient/src/main/java/com/mea/ms/CustomerOperationsController.java
@@ -0,0 +1,23 @@
+package com.mea.ms;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer-api")
+public class CustomerOperationsController {
+
+	@Value("${dbuser}")
+	private String uname;
+	@Value("${dbpwd}")
+	private String upwd;
+	
+	@GetMapping("/cust-data")
+	public ResponseEntity<String> showData(){
+		return new ResponseEntity<String>("Customer MS Data is : Uname : "+uname+" , Pwd : "+upwd,HttpStatus.OK);
+	}
+}
