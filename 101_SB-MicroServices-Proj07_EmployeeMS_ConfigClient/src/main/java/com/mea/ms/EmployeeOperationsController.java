f5f793742576609b0ee9669d5250f9d24023153e
Rahul Kumar Pandey <rp201291.java@gmail.com>
2024-08-31 22:10:29 +0530

    MicroServices : ConfigServer Native Files

---
+++ 101_SB-MicroServices-Proj07_EmployeeMS_ConfigClient/src/main/java/com/mea/ms/EmployeeOperationsController.java
@@ -0,0 +1,23 @@
+package com.mea.ms;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee-api")
+public class EmployeeOperationsController {
+
+	@Value("${dbuser}")
+	private String uname;
+	@Value("${dbpwd}")
+	private String upwd;
+	
+	@GetMapping("/emp-data")
+	public ResponseEntity<String> showData(){
+		return new ResponseEntity<String>("Employee MS Data is : Uname : "+uname+" , Pwd : "+upwd,HttpStatus.OK);
+	}
+}
